21c806e75d46b2a184f83dc0e2ed2f96a436670a
Alexey Dobrynin <ag.dobrynin@gmail.com>
2016-11-08 21:01:03 +0400

    ok
    task 2b

--- src/index.js
+++ src/index.js
@@ -14,6 +14,40 @@ app.get('/task2a', (req, res) => {
   res.send(sum.toString());
 });
 
+app.get('/task2b', (req, res) => {
+
+  var name = req.query.fullname, aname, matches=[];
+  const invalidname = "Invalid fullname";
+
+  if (name == undefined || name == "" || /[0-9_\/]+/g.test(name)){
+    name = invalidname;
+  }else{
+    aname=name.trim().split(" ");
+
+    for(var a in aname){
+      if(aname[a].trim() !== ""){
+         matches[matches.length]=aname[a].charAt(0).toUpperCase() + aname[a].slice(1).toLowerCase();
+      }
+    }
+
+    switch (matches.length) {
+      case 1:
+        name =  matches[0].trim();
+        break;
+      case 2:
+        name =  matches[1].trim() + " " + matches[0].slice(0,1) + ".";
+        break;
+      case 3:
+        name = name = matches[2].trim() + " " + matches[0].slice(0,1) + ". " + matches[1].slice(0,1) + ".";
+        break;
+      default:
+        name = invalidname;
+    }
+  }
+
+  res.send(name);
+});
+
 app.listen(3000, () => {
-  console.log('Your app listening on port 3000!');
+  console.log('Your app listening on port 3000');
 });
